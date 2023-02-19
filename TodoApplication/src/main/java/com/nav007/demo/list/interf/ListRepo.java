a90cb3e1308ee85ec99a116ca034b7899ab898d9
Navneetsfdv <125782139+Navneetsfdv@users.noreply.github.com>
2023-02-19 18:31:45 +0530

    Update ListRepo.java

--- TodoApplication/src/main/java/com/nav007/demo/list/interf/ListRepo.java
+++ TodoApplication/src/main/java/com/nav007/demo/list/interf/ListRepo.java
@@ -12,11 +12,3 @@ public interface ListRepo extends JpaRepository<TodoList, Integer> {
 	
 
 }
-/* </form> 
-<form action ="delete">
-<input type="hidden" name="del" value="<%=s.getId() %>" >
-
-<input type="submit" value="remove">
-
-</form>  */
-//   <input type="text" name ="id">   
